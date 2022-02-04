03dee0a5f126cc8f6e1c0e4a0e85700056638ce9
Russell Gordon <github@russellgordon.ca>
2022-02-04 07:18:59 -0500

    Initial Commit

---
+++ ArithmeticPractice/ArithmeticPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticPracticeApp.swift
+//  ArithmeticPractice
+//
+//  Created by Russell Gordon on 2022-02-04.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
